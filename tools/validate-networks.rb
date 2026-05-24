f1192b260b66515092c3cbe95e54d9d33b5ba11e
Sadie Powell <sadie@sadiepowell.dev>
2026-05-24 18:03:23 +0100

    Re-add the webchat links, this time requiring a WebSocket port.

--- tools/validate-networks.rb
+++ tools/validate-networks.rb
@@ -1,12 +1,13 @@
 require "yaml"
 
 def check_section(hash, sections)
-    sections.each do |section, type|
-        fail "Network list is missing the #{section} section" unless hash.include?(section)
-        fail "The #{section} section is not a #{type}" unless hash[section].is_a?(type)
+    sections.each do |section, types|
+        fail "Network list is missing the #{section} section" unless hash.include?(section) || types.include?(NilClass)
+        fail "The #{section} section is not a #{type}" unless types.any? { |type| hash[section].is_a?(type) }
         fail "The #{section} section is empty" if hash[section].respond_to?(:empty?) && hash[section].empty?
     end
-    fail "Unexpected extra keys: #{hash.keys - sections.keys}" if hash.keys != sections.keys
+    keydiff = hash.keys - sections.keys
+    fail "Unexpected extra keys: #{keydiff}" unless keydiff.empty?
 end
 
 networks = YAML.load_file(File.join(__dir__, "..", "_data", "networks.yml"))
@@ -15,17 +16,18 @@ fail "Network list is not an array" unless networks.is_a?(Array)
 previous_network = nil
 networks.each do |network|
     check_section(network, {
-        "name"        => String,
-        "contact"     => String,
-        "description" => String,
-        "server"      => Hash,
-        "url"         => String,
+        "name"        => [String],
+        "contact"     => [String],
+        "description" => [String],
+        "server"      => [Hash],
+        "url"         => [String],
     })
 
     check_section(network["server"], {
-        "host" => String,
-        "port" => Integer,
-        "chan" => String,
+        "host"   => [String],
+        "port"   => [Integer],
+        "wsport" => [Integer, NilClass],
+        "chan"   => [String],
     })
 
     this_network = network["name"].downcase
